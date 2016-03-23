6c03840837fa13136da2a2c36d3432d4dd6a47e3
michal ondras <m.e.ondras@gmail.com>
2016-03-22 17:18:51 -0700

    Added tooltip to box and fences; Changed title and bats

--- box-JM.js
+++ box-JM.js
@@ -22,7 +22,12 @@
           max = d[n - 1];
 
       // Compute quartiles. Must return exactly 3 elements.
+      // quartiles is a d3 funtion: d3.quartiles([...],.25)
       var quartileData = d.quartiles = quartiles(d);
+      // why does he do ths? quariles(d) or d.quartiles?
+      console.log('quartileData', quartileData);
+      console.log('d.quartiles', d.quartiles);
+      console.log('quartiles(d)', quartiles(d));
 
       // Compute whiskers. Must return exactly 2 elements, or null.
       var whiskerIndices = whiskers && whiskers.call(this, d, i),
@@ -46,6 +51,19 @@
 
       // Stash the new scale.
       this.__chart__ = x1;
+
+      // Define the div for the tooltip for mouse-over outliers
+      var div = d3.select("body").append("div") 
+      .attr("class", "tooltip")       
+      .style("opacity", 0);
+
+      var divB = d3.select("body").append("div") 
+      .attr("class", "tooltipBox")       
+      .style("opacity", 0);
+
+      var table = d3.select("body").append("table") 
+      .attr("class", "tooltipTable")       
+      .style("opacity", 0);
 
       // Note: the box, median, and box tick elements are fixed in number,
       // so we only have to handle enter and update. In contrast, the outliers
@@ -89,14 +107,41 @@
       box.enter().append("rect")
           .attr("class", "box")
           .attr("x", 0)
-          .attr("y", function(d) { return x0(d[2]); })
+          .attr("y", function(d) { return x0(d[2]); })                    // returns 500 the width 
           .attr("width", width)
-          .attr("height", function(d) { return x0(d[0]) - x0(d[2]); })
+          .attr("height", function(d) { return x0(d[0]) - x0(d[2]); })    // returns 0
+          .on("mouseover", function(d) {    
+              divB.transition()    
+                  .duration(200)    
+                  .style("opacity", .9);    
+              divB.html("<table style='width:100%'>"+
+                            "<tr>"+
+                              "<td>"+"75th Quartile: "+"</td>"+
+                              "<td>"+d[2]+"</td>"+
+                            "</tr>" +
+                            "<tr>"+
+                              "<td>"+"Mean: "+"</td>"+
+                              "<td>"+d[1]+"</td>"+
+                            "</tr>" +
+                            "<tr>"+
+                              "<td>"+"25th Quartile: "+"</td>"+
+                              "<td>"+d[0]+"</td>"+
+                            "</tr>" +
+                        "</table>")    
+            //"75th Quartile: "+d[2]+"<br>" + "Mean: "+d[1]+"<br>"+ "25th Quartile: "+d[0])  
+                  .style("left", (d3.event.pageX + 10) + "px")   
+                  .style("top", (d3.event.pageY - 50) + "px");   
+                })
+          .on("mouseout", function(d) {   
+              divB.transition()    
+                  .duration(500)    
+                  .style("opacity", 0); 
+                }) 
         .transition()
           .duration(duration)
           .attr("y", function(d) { return x1(d[2]); })
           .attr("height", function(d) { return x1(d[0]) - x1(d[2]); });
-
+          
       box.transition()
           .duration(duration)
           .attr("y", function(d) { return x1(d[2]); })
@@ -132,7 +177,20 @@
           .attr("y1", x0)
           .attr("x2", width)
           .attr("y2", x0)
-          .style("opacity", 1e-6)
+          .on("mouseover", function(i) {   
+                div.transition()    
+                    .duration(200)    
+                    .style("opacity", .9);    
+                div.html("Fence Value: "+"<br>"+i)  
+                    .style("left", (d3.event.pageX) + "px")   
+                    .style("top", (d3.event.pageY - 28) + "px");  
+                })
+                .on("mouseout", function(d) {   
+                div.transition()    
+                    .duration(500)    
+                    .style("opacity", 0); 
+                })          
+          .style("opacity", 1e-6)               
         .transition()
           .duration(duration)
           .attr("y1", x1)
@@ -151,17 +209,15 @@
           .attr("y2", x1)
           .style("opacity", 1e-6)
           .remove();
-
-      // Define the div for the tooltip
-      var div = d3.select("body").append("div") 
-      .attr("class", "tooltip")       
-      .style("opacity", 0);
-
+     
       // Update outliers.
       var outlier = g.selectAll("circle.outlier")
           .data(outlierIndices, Number);
 
-      // this works ... 
+      /* 
+      call explicit works, whereas in a function it doesn't
+      */
+
       if (d.length===533){
         outlier.enter().insert("circle", "text")
               .attr("class", "outlier")
@@ -242,18 +298,29 @@
  
       // Compute the tick format.
       var format = tickFormat || x1.tickFormat(8);
+      console.log("computing the tickFormat");
+      console.log('tickFormat', tickFormat);
+      console.log('x1.tickFormat', x1.tickFormat(8));
 
       // Update box ticks.
       var boxTick = g.selectAll("text.box")
           .data(quartileData);
 
+      /* 
+        - uses ternary operator: 
+          condition ? expr 1 : expr 2
+        - quartileData is array with 25th, 50th, and 75th percentile 
+        - turned off
+      */
+      if (false) {
       boxTick.enter().append("text")
           .attr("class", "box")
           .attr("dy", ".3em")
-          .attr("dx", function(d, i) { return i & 1 ? 6 : -6 })
-          .attr("x", function(d, i) { return i & 1 ? width : 0 })
+          .attr("dx", function(d, i) { return i & 1 ? 6 : -6 })   //returns +6pixels(1) or -6pixels (0,2)
+          .attr("x", function(d, i) { return i & 1 ? width : 0 }) //returns width(1) or zero (0,2)
           .attr("y", x0)
-          .attr("text-anchor", function(d, i) { return i & 1 ? "start" : "end"; })
+          .attr("text-anchor", function(d, i) { return i & 1 ? "start" : "end"; })                                
+          //anchors text @ start(1) or end (0,2). 
           .text(format)
         .transition()
           .duration(duration)
@@ -263,10 +330,13 @@
           .duration(duration)
           .text(format)
           .attr("y", x1);
+      };
 
       // Update whisker ticks. These are handled separately from the box
       // ticks because they may or may not exist, and we want don't want
       // to join box ticks pre-transition with whisker ticks post-.
+      
+      if (false){
       var whiskerTick = g.selectAll("text.whisker")
           .data(whiskerData || []);
 
@@ -294,6 +364,9 @@
           .attr("y", x1)
           .style("opacity", 1e-6)
           .remove();
+
+    };
+
     });
     
     d3.timer.flush();
